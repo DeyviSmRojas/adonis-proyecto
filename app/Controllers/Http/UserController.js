000a1ba8cec923035abfd1f7aaf29189260c7efa
DeyviSR <salomerojasdeyvi@gmail.com>
2020-02-07 19:18:07 -0500

    Ruta creada de login para mostrar el token

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,6 +3,15 @@
 const User = use('App/Models/User')
 
 class UserController {
+
+    //Creando el Login
+    async login({request, auth}){
+        const {username, email, password} = request.all();
+        const token = await auth.attempt(username, password);
+        return token;
+    }
+    
+    //Creando Registro
     async store({request}){
         const {username, email, password} = request.all();
         const user = await User.create({
